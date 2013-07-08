2ea5184b35dc63a6551ac952044e0d22e083fc83
sacrac <crocha09.09@gmail.com>
2013-07-08 16:31:21 -0600

    fix setting

--- sitioadic/settings.py
+++ sitioadic/settings.py
@@ -90,6 +90,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
+    'suit',
     'django.contrib.admin',
     'django.contrib.flatpages',
     # Uncomment the next line to enable admin documentation:
